41b010a66c080842fc48d4fdd3f88abc666c236d
Cathersal <cathersa@amazon.com>
2019-06-03 21:21:27 -0700

    test3

--- PowerShell/WIGValidationWindows-Pester.tests.ps1
+++ PowerShell/WIGValidationWindows-Pester.tests.ps1
@@ -6,17 +6,17 @@ inmodulescope WIGValidationWindows-Module   {
     Describe "Get-FreeDiskSpace" {
 
         it 'Returns Pass' {
-            mock Get-WmiObject -MockWith {[PSCustomObject] @{'freespace' = "381770240000"}}
+            mock get-ciminstance -MockWith {[PSCustomObject] @{'freespace' = "381770240000"}}
             (Get-FreeDiskSpace -DriveLetter C -MinimumSizeInGB 10).result | Should be "Pass"
         }
 
         it 'Returns Fail' {
-            mock Get-WmiObject -MockWith {[PSCustomObject] @{'freespace' = "240000"}}
+            mock get-ciminstance -MockWith {[PSCustomObject] @{'freespace' = "240000"}}
             (Get-FreeDiskSpace -DriveLetter C -MinimumSizeInGB 10).result | Should be "Fail"
         }
 
         it 'Throws an error' {
-            mock Get-WmiObject -MockWith {[PSCustomObject] @{'freespace' = "xx"}}
+            mock get-ciminstance -MockWith {[PSCustomObject] @{'freespace' = "xx"}}
          {Get-FreeDiskSpace -DriveLetter C -MinimumSizeInGB 10} |  Should -Throw
         }
 
