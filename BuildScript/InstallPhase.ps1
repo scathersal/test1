705ed42dd7a639bd61bb0522ae889107b2c664f4
Cathersal <cathersa@amazon.com>
2019-06-04 15:04:34 -0700

    install new version of pester2

--- BuildScript/InstallPhase.ps1
+++ BuildScript/InstallPhase.ps1
@@ -2,6 +2,6 @@
 $PSVersionTable
 
 Install-Module -Name psake -Force
-install-Module -Name pester -SkipPublisherCheck
+install-Module -Name pester -SkipPublisherCheck -Force
 Install-Module -Name PSScriptAnalyzer -Force
 # Install-Module -Name AWSPowerShell.netcore -Force
